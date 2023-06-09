c6b26fcedd82d39a2b0023ae846d723ea7b1c263
rifqin11 <109424005+rifqin11@users.noreply.github.com>
2023-06-09 07:06:40 +0700

    fix multiple jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,18 +8,19 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float movementSpeed;
     [SerializeField] private float jumpForce;
     private bool isJumping;
+    private bool isGrounded;
     private float horiz;
 
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody = GetComponent<Rigidbody2D>();;
     }
 
     void Update()
     {
         horiz = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump") && !isJumping)
+        if (Input.GetButtonDown("Jump") && !isJumping && isGrounded)
         {
             isJumping = true;
         }
@@ -33,6 +34,23 @@ public class PlayerController : MonoBehaviour
         {
             isJumping = false;
             rigidBody.AddForce(new Vector2(0, jumpForce * 10));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+
         }
     }
 }
