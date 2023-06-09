8257794a1bee52849def92778dae6d8d4e6d7315
Dhik <andikamuhammad48@gmail.com>
2023-06-10 00:33:02 +0700

    chnage 10

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
-    // Start is called before the first frame update
+    /* Start is called before the first frame update
     public Joystick Lompat;
     public float Force;
-    bool Lompatcuy = false;
+    bool Lompatcuy = false;*/
 
     // Update is called once per frame
     void Update()
     {
-        float verticalMove = Lompat.Vertical;
+        /*float verticalMove = Lompat.Vertical;
 
         if (verticalMove >= .5f)
         {
             Lompatcuy = true;
-        }
+        }*/
     }
 }
